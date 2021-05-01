0c91c6e37fc39a18dd6840d892d593ffde411d0b
Si-An <sian1_chen@asus.com>
2021-05-01 06:13:01 +0000

    update KimCNNv2

--- networks/kim_cnn_v2.py
+++ networks/kim_cnn_v2.py
@@ -11,19 +11,17 @@ class KimCNNv2(BaseModel):
 
         self.filter_sizes = config.filter_sizes
         emb_dim = embed_vecs.shape[1]
-        num_filter_maps = config.num_filter_maps
-        strides = config.strides
+        num_filter_per_size = config.num_filter_per_size
 
         self.convs = nn.ModuleList()
 
-        for filter_size, stride in zip(self.filter_sizes, strides):
-            conv = nn.Conv2d(
-                in_channels=1,
-                out_channels=num_filter_maps,
-                kernel_size=(filter_size, emb_dim),
-                stride=(stride, emb_dim))
+        for filter_size in self.filter_sizes:
+            conv = nn.Conv1d(
+                in_channels=emb_dim,
+                out_channels=num_filter_per_size,
+                kernel_size=filter_size)
             self.convs.append(conv)
-        conv_output_size = num_filter_maps * len(self.filter_sizes)
+        conv_output_size = num_filter_per_size * len(self.filter_sizes)
 
         self.dropout2 = nn.Dropout(config.dropout2)
         self.linear1 = nn.Linear(conv_output_size, config.hidden_dim)
@@ -32,12 +30,11 @@ class KimCNNv2(BaseModel):
     def forward(self, text):
         h = self.embedding(text) # (batch_size, length, embed_dim)
         h = self.embed_drop(h)
-        h = h.view(h.shape[0], 1, h.shape[1], h.shape[2]) # (batch_size, 1, length, embed_dim)
+        h = h.transpose(1, 2) # (batch_size, embed_dim, length)
 
         h_list = []
         for conv in self.convs:
-            h_sub = conv(h) # (batch_size, num_filter, H, 1)
-            h_sub = h_sub.view(h_sub.shape[0], h_sub.shape[1], -1) # (batch_size, num_filter, H)
+            h_sub = conv(h) # (batch_size, num_filter, length)
             h_sub = F.max_pool1d(h_sub, kernel_size=h_sub.size()[2]) # (batch_size, num_filter, 1)
             h_sub = h_sub.view(h_sub.shape[0], -1) # (batch_size, num_filter)
             h_list.append(h_sub)
@@ -46,10 +43,10 @@ class KimCNNv2(BaseModel):
             h = torch.cat(h_list, 1)
         else:
             h = h_list[0]
-        h = F.relu(h) # (batch_size, N * num_filter)
+        h = self.activation(h) # (batch_size, N * num_filter)
 
         # linear output
-        h = F.relu(self.linear1(h))
+        h = self.activation(self.linear1(h))
         h = self.dropout2(h)
         h = self.linear2(h)
         return {'logits': h}
