fb9ded84861900d83b17f79d2dd98979ed7c0233
Si-An Chen <sian1_chen@asus.com>
2021-06-07 04:43:38 +0000

    enable multi-filter-size for CAML

--- libmultilabel/networks/caml.py
+++ libmultilabel/networks/caml.py
@@ -12,41 +12,53 @@ class CAML(BaseModel):
     def __init__(self, config, embed_vecs):
         super(CAML, self).__init__(config, embed_vecs)
 
-        if len(config.filter_sizes) != 1:
-            raise ValueError(f'CAML expect 1 filter size. Got filter_sizes={config.filter_sizes}')
-        filter_size = config.filter_sizes[0]
+        emb_dim = embed_vecs.shape[1]
 
-        num_filter_per_size = config.num_filter_per_size
-
-        # initialize conv layer as in 2.1
-        self.conv = nn.Conv1d(embed_vecs.shape[1], num_filter_per_size, kernel_size=filter_size, padding=int(floor(filter_size/2)))
-        xavier_uniform_(self.conv.weight)
+        self.convs = nn.ModuleList()
+        for filter_size in config.filter_sizes:
+            conv = nn.Conv1d(
+                in_channels=emb_dim,
+                out_channels=config.num_filter_per_size,
+                kernel_size=filter_size,
+                padding=(filter_size // 2),
+            )
+            xavier_uniform_(conv.weight)
+            self.convs.append(conv)
+        total_conv_size = len(self.convs) * config.num_filter_per_size
 
         # Context vectors for computing attention as in 2.2
-        self.U = nn.Linear(num_filter_per_size, config.num_classes)
+        self.U = nn.Linear(total_conv_size, config.num_classes)
         xavier_uniform_(self.U.weight)
 
         # Final layer: create a matrix to use for the L binary classifiers as in 2.3
-        self.final = nn.Linear(num_filter_per_size, config.num_classes)
+        self.final = nn.Linear(total_conv_size, config.num_classes)
         xavier_uniform_(self.final.weight)
 
     def forward(self, text):
         # Get embeddings and apply dropout
-        x = self.embedding(text)
-        x = self.embed_drop(x)
-        x = x.transpose(1,2)
+        h = self.embedding(text)
+        h = self.embed_drop(h)
+        h = h.transpose(1,2)
 
-        # Apply convolution and nonlinearity (tanh / relu)
-        x = self.activation(self.conv(x).transpose(1,2))
+        h_list = []
+        for conv in self.convs:
+            h_sub = conv(h) # (batch_size, num_filter, length)
+            h_list.append(h_sub)
+
+        if len(self.convs) > 1:
+            h = torch.cat(h_list, 1)
+        else:
+            h = h_list[0]
+        # Max-pooling and monotonely increasing non-linearities commute. Here
+        # we apply the activation function after max-pooling for better
+        # efficiency.
+        h = self.activation(h) # (batch_size, tot_num_filter, length)
 
         # Apply attention
-        #    batch * text_length * 1200
-        # -> batch * text_length * num_class (matmul)
-        # -> batch * num_class (softmax)
-        alpha = torch.softmax(self.U.weight.matmul(x.transpose(1,2)), dim=2)
+        alpha = torch.softmax(self.U.weight.matmul(h), dim=2) # (batch_size, num_class, length)
 
         # Document representations are weighted sums using the attention. Can compute all at once as a matmul
-        m = alpha.matmul(x)
+        m = alpha.matmul(h.transpose(1, 2)) # (batch_size, num_class, tot_num_filter)
 
         # similarity
         x = self.final.weight.mul(m).sum(dim=2).add(self.final.bias)
